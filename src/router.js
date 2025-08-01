7df938577175909360f8fba6ae00449f90221138
Aditya972307 <aditya972307@gmail.com>
2025-08-01 11:33:50 +0700

    Initial commit

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import Utama from './Layout/Utama.vue'
 import Beranda from './Menu/Beranda.vue'
 
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
